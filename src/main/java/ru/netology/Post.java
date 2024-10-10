e90e0503a94ad9c5b1c82448362ff1bc45ea954b
Dmitriy Mekheda <mekhedadima@gmail.com>
2024-10-10 10:19:07 +0500

    task completed

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String pastronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public class FormDate {
+        public static int day;
+        public static int month;
+        public static int year;
+
+    }
+}
